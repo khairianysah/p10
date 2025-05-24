cdb049f58014b243db9b9f911f22127cb4796d5b
khairiansyah <ansyahkhairi8@gmail.com>
2025-05-24 19:04:32 +0700

    Fix Vercel deploy: add base config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './', // ← ini penting agar Vercel tidak tampil blank
 })
